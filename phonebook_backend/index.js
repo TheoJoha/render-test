c066f972ea7378ab365cf7fe53825228cf1ddde1
TheoJoha <96450966+TheoJoha@users.noreply.github.com>
2023-01-26 15:07:22 +0100

    Update index.js

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -129,7 +129,7 @@ morgan.token('body', req => {
   })
   
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Sample app listening at http://localhost:${PORT}`)
 })
